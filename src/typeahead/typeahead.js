9b050813d12b9da97ed883c396e3b8989dfd6aad
Oron Nadiv <oron.nadiv@lanetix.com>
2015-03-05 16:46:23 -0800

    fix(typeahead): lanetix/issues#1898

--- src/typeahead/typeahead.js
+++ src/typeahead/typeahead.js
@@ -134,6 +134,9 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
           //but we are interested only in responses that correspond to the current view value
           var onCurrentRequest = (inputValue === modelCtrl.$viewValue);
           if (onCurrentRequest && hasFocus) {
+            if (!matches) {
+              return;
+            }
             if (matches.length > 0) {
 
               scope.activeIdx = 0;
